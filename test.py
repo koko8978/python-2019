a75dec739207c2ddbfd1c50dbade707751733b13
koko8978 <55995377+koko8978@users.noreply.github.com>
2019-10-01 03:40:34 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import os
 os.system('clear')
-nickname="Babi"
-age="21"
-name=["babi","bob","Bobi"]
-pizza={
-    "babi":"cheese",
-    "bob":"mah",
-    "bobi":"tahia"
-}
-n= True
-print(n)
+n=1
+fc=0
+bc=0
+fbc=0
+while (n<=100):
+    if(n%3==0)and(n%5==0):
+        print(str(n)+".Fizzbuzz")
+        fbc+=1
+    elif (n%3==0):
+        print(str(n)+"fizz")
+        fc+=1
+    elif (n%5==0):
+        print(str(n)+".buzz")
+        bc+=1
 
+    n+=1
+print("________________________________________")
+print("Fizz\tBuzz\tFiizBuzz")
+print ( str(fc) + '\t' + str(bc)+ '\t' + str(fbc) )
